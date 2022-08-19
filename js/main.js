570d964c61dbe7b44ae51da6cb4ccab023183a25
Daniel Barrera <daniel_sbarrera@hotmail.com>
2022-08-18 22:39:05 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,52 +1,42 @@
-var moveForce = 30; // max popup movement in pixels
-var rotateForce = 20; // max popup rotation in deg
+const moveForce = 40; // max popup movement in pixels
+const rotateForce = 15; // max popup rotation in deg
 
 $(document).mousemove(function(e) {
 
-    var docX = $(document).width();
-    var docY = $(document).height();
+    let docX = $(document).width();
+    let docY = $(document).height();
     
-    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
-    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+    let moveX = (docX - e.pageX) / (docX) * -moveForce + 150;
+    let moveY = (e.pageY - docY) / (docY) * -moveForce - 30;
     
-    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
-    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
-    
+    // let rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
+    // let rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
+
     $('#logo')
         .css('left', moveX+'px')
         .css('top', moveY+'px')
-        .css('transform', 'rotateY('+rotateY+'deg)');
-
-        /* rotateX('+rotateX+'deg) */
+        // .css('transform', 'rotateY('+rotateY+'deg) rotateX('+rotateX+'deg)');
 
 });
 
 
 jQuery(document).ready(function($){
     
-
-    
-
     const timeAnimate = 1500
     $("#header").animate({
         "top": "0" 
-    }, timeAnimate*(3/2));
-    $(".desc-nombre").hide();
-    $(".desc-nombre").fadeIn(timeAnimate/2, function() {
+    }, timeAnimate);
+
+
+    $(".desc-nombre").animate({
+        "opacity": "1",
+    } ,timeAnimate, function() {
 
         $(".desc-texto").animate({
             "left": "0rem",
             "opacity": "1",
         },timeAnimate)
+
     })
 
-
-    
-
-    
-    
-    
-    
-    
-    
 })
