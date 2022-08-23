21187b6bdb5fee867abb94ae9dd810949e61e3a6
Daniel Barrera <daniel_sbarrera@hotmail.com>
2022-08-23 00:12:40 -0500

    update

---
+++ js/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        tittle: "Renovacion de la pagina web UIS",
+        bgUrl: "../images/img-uis.webp",
+        desciption: "Pagina web de la Univesidad Industrial de Santander"
+    },
+    {
+        tittle: "Dimensionamiento de microrredes aisladas",
+        bgUrl: "../images/img-PdG.webp",
+        desciption: "uso de Maching learning el analisis de microrredes aisladas de generacion electrica"
+    },
+    {
+        tittle: "Frases aleatorias",
+        bgUrl: "../images/img-project.webp",
+        desciption: "Frases celebres aleatorias (y twitteables!)"
+    },
+    {
+        tittle: "editor de Markdown",
+        bgUrl: "../images/img-project.webp",
+        desciption: "Redacta en sintaxis Markdown en este editor."
+    },
+    {
+        tittle: "Percusion",
+        bgUrl: "../images/img-project.webp",
+        desciption: "Toca algunas notas en esta web."
+    },
+    {
+        tittle: "Buscador de Gifs",
+        bgUrl: "../images/img-project.webp",
+        desciption: "Busca algunos gifs en esta pagina web!"
+    },
+];
