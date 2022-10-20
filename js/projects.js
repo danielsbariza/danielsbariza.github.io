925bccd9b8cd3646eea5c9ce8da1937b776a8cc6
Daniel Barrera <daniel_sbarrera@hotmail.com>
2022-10-19 22:27:00 -0500

    update

--- js/projects.js
+++ js/projects.js
@@ -10,6 +10,27 @@ export const languages = {
 
 
 export const projects = [
+    // {
+    //     tittle: "editor de Markdown",
+    //     bgUrl: "../images/img-markdown.webp",
+    //     url: "https://cdpn.io/pen/debug/eYMwjBd?authentication_hash=wQAPobJNXOwr",
+    //     desciption: "Redacta en sintaxis Markdown en este editor.",
+    //     language: [
+    //         languages.html,
+    //         languages.css,
+    //         languages.javascript,
+    //         languages.react,
+    //     ],
+    // },
+    {
+        tittle: "Dimensionamiento de microrredes aisladas a partir de técnicas de regresión de aprendizaje automático",
+        bgUrl: "../images/img-PdG.webp",
+        url: "https://github.com/danielsbariza/TesisDeGrado",
+        desciption: "En este proyecto se propone una metodología alternativa para el dimensionamiento de microrredes aisladas de generación eléctrica en  condiciones climáticas y de carga especificas utilizando técnicas de regresión de aprendizaje automático.<br>Se busca evaluar su desempeño con respecto a la formulación clásica y proponer un procedimiento para evaluar la relación entre la precisión de los modelos y la proporción de los datos usados.",
+        language: [
+            languages.python,
+        ],
+    },
     {
         tittle: "Renovacion de la pagina web de la Universidad Industrial de Santander",
         bgUrl: "../images/img-uis.webp",
@@ -22,28 +43,6 @@ export const projects = [
             languages.wordpress,
         ],
     },
-    {
-        tittle: "editor de Markdown",
-        bgUrl: "../images/img-markdown.webp",
-        url: "https://cdpn.io/pen/debug/eYMwjBd?authentication_hash=wQAPobJNXOwr",
-        desciption: "Redacta en sintaxis Markdown en este editor.",
-        language: [
-            languages.html,
-            languages.css,
-            languages.javascript,
-            languages.react,
-        ],
-    },
-    {
-        tittle: "Dimensionamiento de microrredes aisladas a partir de técnicas de regresión de aprendizaje automático",
-        bgUrl: "../images/img-PdG.webp",
-        url: "https://github.com/danielsbariza/TesisDeGrado",
-        desciption: "En este proyecto se propone una metodología alternativa para el dimensionamiento de microrredes aisladas de generación eléctrica en  condiciones climáticas y de carga especificas utilizando técnicas de regresión de aprendizaje automático.<br>Se busca evaluar su desempeño con respecto a la formulación clásica y proponer un procedimiento para evaluar la relación entre la precisión de los modelos y la proporción de los datos usados.",
-        language: [
-            languages.python,
-        ],
-    },
-
 
     // {
     //     tittle: "Frases aleatorias",
