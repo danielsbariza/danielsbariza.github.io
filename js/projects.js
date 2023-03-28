10583acbf02cd9df9e8747c1ae1115c6654c3dad
danielsbariza <daniel_sbarrera@hotmail.com>
2023-03-28 17:28:40 -0500

    Update

--- js/projects.js
+++ js/projects.js
@@ -23,7 +23,7 @@ export const projects = [
     //     ],
     // },
     {
-        tittle: "Dimensionamiento de microrredes aisladas a partir de técnicas de regresión de aprendizaje automático",
+        tittle: "Dimensionamiento de microrredes aisladas a partir de técnicas de regresión de Machine Learning",
         bgUrl: "../images/img-PdG.webp",
         url: "https://github.com/danielsbariza/TesisDeGrado",
         desciption: "En este proyecto se propone una metodología alternativa para el dimensionamiento de microrredes aisladas de generación eléctrica en  condiciones climáticas y de carga especificas utilizando técnicas de regresión de aprendizaje automático.<br>Se busca evaluar su desempeño con respecto a la formulación clásica y proponer un procedimiento para evaluar la relación entre la precisión de los modelos y la proporción de los datos usados.",
@@ -35,7 +35,7 @@ export const projects = [
         tittle: "Renovacion de la pagina web de la Universidad Industrial de Santander",
         bgUrl: "../images/img-uis.webp",
         url: "https://uis.edu.co",
-        desciption: "Para este proyecto, me desempeño como auxiliar del equipo de trabajo Character TEAM, donde el principal objetivo es la renovación, desarrollo y diseño de la nueva página web de la Universidad Industrial de Santander.",
+        desciption: "Para este proyecto, me desempeñé como auxiliar del equipo de trabajo Character TEAM, donde el principal objetivo es la renovación, desarrollo y diseño de la nueva página web de la Universidad Industrial de Santander.",
         language: [
             languages.html,
             languages.css,
@@ -44,11 +44,14 @@ export const projects = [
         ],
     },
 
-    // {
-    //     tittle: "Frases aleatorias",
-    //     bgUrl: "../images/img-project.webp",
-    //     desciption: "Frases celebres aleatorias (y twitteables!)"
-    // },
+    {
+        tittle: "Aprendiz Analista de Datos Mibanco",
+        bgUrl: "../images/img-project.webp",
+        desciption: "Extracción, Transformación y Carga (ETL) de informacion por medio de Python, Excel y Power BI para analizar, preveer y mitigar los incumplimientos en los lineamientos para la ortorgación de crédito.",
+        language: [
+            languages.python,
+        ],
+    },
 
     // {
     //     tittle: "Percusion",
